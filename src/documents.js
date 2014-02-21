r4f4/gnome-documents

commit 78a41e8e9b8fc39aef7cc2d314f7cb9a625f492c

Protect against spurious view-as signals
Do not let the GAction and GSettings unnecessarily change each others
state and value when there has been no change.

Listen to the GAction's state for actually changing the UI, because
someone using dconf-editor will still be able to emit spurious
changed::view-as signals from GSettings.

https://bugzilla.gnome.org/show_bug.cgi?id=723690

src/documents.js
@@ -248,8 +248,8 @@ const DocCommon = new Lang.Class({
         this.populateFromCursor(cursor);
 
         this._refreshIconId =
-            Application.settings.connect('changed::view-as',
-                                         Lang.bind(this, this.refreshIcon));
+            Application.application.connect('action-state-changed::view-as',
+                                            Lang.bind(this, this.refreshIcon));
         this._filterId =
             Application.searchCategoryManager.connect('active-changed',
                                                       Lang.bind(this, this.refreshIcon));

src/application.js
@@ -160,7 +160,9 @@ const Application = new Lang.Class({
     _viewAsCreateHook: function(action) {
         settings.connect('changed::view-as', Lang.bind(this,
             function() {
-                action.state = settings.get_value('view-as');
+                let state = settings.get_value('view-as');
+                if (state.get_string()[0] != action.state.get_string()[0])
+                    action.state = state;
             }));
     },
 
@@ -187,7 +189,8 @@ const Application = new Lang.Class({
     },
 
     _onActionViewAs: function(action, parameter) {
-        settings.set_value('view-as', parameter);
+        if (parameter.get_string()[0] != action.state.get_string()[0])
+            settings.set_value('view-as', parameter);
     },
 
     _onActionOpenCurrent: function() {

src/view.js
@@ -206,7 +206,7 @@ const ViewContainer = new Lang.Class({
                             Lang.bind(this, this._onViewSelectionChanged));
 
         // connect to settings change for list/grid view
-        this._viewSettingsId = Application.settings.connect('changed::view-as',
+        this._viewSettingsId = Application.application.connect('action-state-changed::view-as',
             Lang.bind(this, this._updateTypeForSettings));
         this._updateTypeForSettings();
 

commit 20cce34879d51d31e730a685803eb8130254c7bd

documents: Disconnect the handler from the correct instance
Fallout from 78a41e8e9b8fc39aef7cc2d314f7cb9a625f492c

https://bugzilla.gnome.org/show_bug.cgi?id=723690

src/documents.js
@@ -546,7 +546,7 @@ const DocCommon = new Lang.Class({
             this._collectionIconWatcher = null;
         }
 
-        Application.settings.disconnect(this._refreshIconId);
+        Application.application.disconnect(this._refreshIconId);
         Application.searchCategoryManager.disconnect(this._filterId);
     },
 