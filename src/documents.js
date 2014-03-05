2345619c65ad20a1f337243d6ca3eac9a832e0df
Debarshi Ray <debarshir@gnome.org>
2014-03-05 17:19:07 +0100

    documents: Fix typo
    Fallout from 6ad46e55572b28da3a4e27ce21bfccb2a7e5310d and
    051683b7f48356d727cad4e771865b5089076808

--- src/documents.js
+++ src/documents.js
@@ -354,11 +354,11 @@ const DocCommon = new Lang.Class({
         }
     },
 
-    canShare: function(callback) {
+    canShare: function() {
         log('Error: DocCommon implementations must override canShare');
     },
 
-    canTrash: function(callback) {
+    canTrash: function() {
         log('Error: DocCommon implementations must override canTrash');
     },
 

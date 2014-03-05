r4f4/gnome-documents

commit 6ad46e55572b28da3a4e27ce21bfccb2a7e5310d

documents: Warn that DocCommon implementations must override canTrash
https://bugzilla.gnome.org/show_bug.cgi?id=725128

src/documents.js
@@ -358,6 +358,10 @@ const DocCommon = new Lang.Class({
         log('Error: DocCommon implementations must override canShare');
     },
 
+    canTrash: function(callback) {
+        log('Error: DocCommon implementations must override canTrash');
+    },
+
     createThumbnail: function(callback) {
         log('Error: DocCommon implementations must override createThumbnail');
     },

commit 2345619c65ad20a1f337243d6ca3eac9a832e0df

documents: Fix typo
Fallout from 6ad46e55572b28da3a4e27ce21bfccb2a7e5310d and
051683b7f48356d727cad4e771865b5089076808

src/documents.js
@@ -354,11 +354,11 @@ const DocCommon = new Lang.Class({
         }
     },
 
-    canShare: function(callback) {
+    canShare: function() {
         log('Error: DocCommon implementations must override canShare');
     },
 
-    canTrash: function(callback) {
+    canTrash: function() {
         log('Error: DocCommon implementations must override canTrash');
     },
 