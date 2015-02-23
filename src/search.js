r4f4/gnome-documents

commit 48fbfc1559975c2bcd58c0c915089649ee68506b

search: Add a helper to get all document SearchTypes
We will need it to construct the queries for the document view.

https://bugzilla.gnome.org/show_bug.cgi?id=686461

src/search.js
@@ -234,6 +234,17 @@ const SearchTypeManager = new Lang.Class({
         return [ activeItem ];
     },
 
+    getDocumentTypes: function() {
+        let types = [];
+
+        types.push(this.getItemById(SearchTypeStock.PDF));
+        types.push(this.getItemById(SearchTypeStock.PRESENTATIONS));
+        types.push(this.getItemById(SearchTypeStock.SPREADSHEETS));
+        types.push(this.getItemById(SearchTypeStock.TEXTDOCS));
+
+        return types;
+    },
+
     getAllTypes: function() {
         let types = [];
 

commit 0a29c2931776a1b899b26a4e68236cc7eeb985b7

search: Unbreak books mode
Fallout from 48fbfc1559975c2bcd58c0c915089649ee68506b

src/search.js
@@ -242,10 +242,15 @@ const SearchTypeManager = new Lang.Class({
     getDocumentTypes: function() {
         let types = [];
 
-        types.push(this.getItemById(SearchTypeStock.PDF));
-        types.push(this.getItemById(SearchTypeStock.PRESENTATIONS));
-        types.push(this.getItemById(SearchTypeStock.SPREADSHEETS));
-        types.push(this.getItemById(SearchTypeStock.TEXTDOCS));
+        if (!Application.application.isBooks) {
+            types.push(this.getItemById(SearchTypeStock.PDF));
+            types.push(this.getItemById(SearchTypeStock.PRESENTATIONS));
+            types.push(this.getItemById(SearchTypeStock.SPREADSHEETS));
+            types.push(this.getItemById(SearchTypeStock.TEXTDOCS));
+        } else {
+            types.push(this.getItemById(SearchTypeStock.EBOOKS));
+            types.push(this.getItemById(SearchTypeStock.COMICS));
+        }
 
         return types;
     },