r4f4/gnome-documents

commit db6fc2e9f4bfd47c4ee73d8d33e9a1efbdc21d99

windowMode: Clean up the fullscreen code
Ensure that the fullscreen logic runs after this._mode has been updated
so that there are no state changes between can-fullscreen-changed and
window-mode-changed.

This changes the current requirement that setFullscreen has to be used
before the mode is updated, which I found to be confusing, and we
don't need a separate this._canFullscreen variable any more.

https://bugzilla.gnome.org/show_bug.cgi?id=686461

src/windowMode.js
@@ -39,12 +39,7 @@ const ModeController = new Lang.Class({
     _init: function() {
         this._mode = WindowMode.NONE;
         this._fullscreen = false;
-        this._canFullscreen = false;
         this._history = [];
-    },
-
-    _canFullscreenPolicy: function(mode) {
-        return (mode == WindowMode.PREVIEW || mode == WindowMode.EDIT);
     },
 
     goBack: function() {
@@ -52,14 +47,12 @@ const ModeController = new Lang.Class({
         if (!oldMode || oldMode == WindowMode.NONE)
             return;
 
-        let policy = this._canFullscreenPolicy(oldMode);
-        this._setCanFullscreen(policy);
-
         // Swap the old and current modes.
         let tmp = oldMode;
         oldMode = this._mode;
         this._mode = tmp;
 
+        this._updateFullscreen();
         this.emit('window-mode-changed', this._mode, oldMode);
     },
 
@@ -69,12 +62,10 @@ const ModeController = new Lang.Class({
         if (oldMode == mode)
             return;
 
-        let policy = this._canFullscreenPolicy(mode);
-        this._setCanFullscreen(policy);
-
         this._history.push(oldMode);
         this._mode = mode;
 
+        this._updateFullscreen();
         this.emit('window-mode-changed', this._mode, oldMode);
     },
 
@@ -82,11 +73,9 @@ const ModeController = new Lang.Class({
         return this._mode;
     },
 
-    _setCanFullscreen: function(canFullscreen) {
-        this._canFullscreen = canFullscreen;
-
-        if (!this._canFullscreen && this._fullscreen)
-            this.setFullscreen(false);
+    _updateFullscreen: function() {
+        if (!this.getCanFullscreen() && this._fullscreen)
+            this._setFullscreen(false);
 
         this.emit('can-fullscreen-changed');
     },
@@ -108,7 +97,7 @@ const ModeController = new Lang.Class({
     },
 
     getCanFullscreen: function() {
-        return this._canFullscreen;
+        return (this._mode == WindowMode.PREVIEW || this._mode == WindowMode.EDIT);
     }
 });
 Signals.addSignalMethods(ModeController.prototype);

commit 008e95f1b48ace8ed5e460c96469c065985b3218

windowMode: Fix typo in method name
The name of the method is setFullscreen, not _setFullscreen - outcome
of a broken rebase.

Fallout from db6fc2e9f4bfd47c4ee73d8d33e9a1efbdc21d99

src/windowMode.js
@@ -84,7 +84,7 @@ const ModeController = new Lang.Class({
 
     _updateFullscreen: function() {
         if (!this.getCanFullscreen() && this._fullscreen)
-            this._setFullscreen(false);
+            this.setFullscreen(false);
 
         this.emit('can-fullscreen-changed');
     },