r4f4/gnome-documents

commit 53440cfe76440a759bd44385d45d07801e9f871b

tracker-controller: fold query error emission in TrackerController
No need to have an ErrorHandler separate object anymore.

src/embed.js
@@ -131,6 +131,8 @@ const Embed = new Lang.Class({
                                       Lang.bind(this, this._onFullscreenChanged));
         Global.trackerController.connect('query-status-changed',
                                          Lang.bind(this, this._onQueryStatusChanged));
+        Global.trackerController.connect('query-error',
+                                         Lang.bind(this, this._onQueryError));
 
         Global.documentManager.connect('active-changed',
                                        Lang.bind(this, this._onActiveItemChanged));
@@ -153,6 +155,10 @@ const Embed = new Lang.Class({
         } else {
             this._spinnerBox.moveOut();
         }
+    },
+
+    _onQueryError: function(manager, message, exception) {
+        this._setError(message, exception.message);
     },
 
     _onFullscreenChanged: function(controller, fullscreen) {
@@ -233,23 +239,10 @@ const Embed = new Lang.Class({
         this._spinnerBox.moveOut();
         this._errorBox.moveOut();
 
-        this._queryErrorId =
-            Global.errorHandler.connect('query-error',
-                                        Lang.bind(this, this._onQueryError));
-
         this._notebook.set_current_page(this._viewPage);
     },
 
-    _onQueryError: function(manager, message, exception) {
-        this._setError(message, exception.message);
-    },
-
     _prepareForPreview: function() {
-        if (this._queryErrorId != 0) {
-            Global.errorHandler.disconnect(this._queryErrorId);
-            this._queryErrorId = 0;
-        }
-
         this._notebook.set_current_page(this._previewPage);
     },
 

src/Makefile-js.am
@@ -4,7 +4,6 @@ dist_js_DATA = \
     changeMonitor.js \
     documents.js \
     embed.js \
-    error.js \
     errorBox.js \
     gDataMiner.js \
     global.js \

src/application.js
@@ -36,7 +36,6 @@ const Tracker = imports.gi.Tracker;
 
 const ChangeMonitor = imports.changeMonitor;
 const Documents = imports.documents;
-const Error = imports.error;
 const Format = imports.format;
 const GDataMiner = imports.gDataMiner;
 const Global = imports.global;
@@ -197,7 +196,6 @@ const Application = new Lang.Class({
 
         Global.changeMonitor = new ChangeMonitor.TrackerChangeMonitor();
         Global.documentManager = new Documents.DocumentManager();
-        Global.errorHandler = new Error.ErrorHandler();
         Global.trackerController = new TrackerController.TrackerController();
         Global.selectionController = new Selections.SelectionController();
         Global.modeController = new WindowMode.ModeController();

src/error.js
@@ -1,42 +1,0 @@
-/*
- * Copyright (c) 2011 Red Hat, Inc.
- *
- * Gnome Documents is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * Gnome Documents is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with Gnome Documents; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * Author: Cosimo Cecchi <cosimoc@redhat.com>
- *
- */
-
-const Lang = imports.lang;
-const Signals = imports.signals;
-
-const Gio = imports.gi.Gio;
-const _ = imports.gettext.gettext;
-
-const ErrorHandler = new Lang.Class({
-    Name: 'ErrorHandler',
-
-    _init: function() {
-    },
-
-    addQueryError: function(exception) {
-        if (exception.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
-            return;
-
-        let message = _("Unable to fetch the list of documents");
-        this.emit('query-error', message, exception);
-    }
-});
-Signals.addSignalMethods(ErrorHandler.prototype);

src/global.js
@@ -31,7 +31,6 @@ let collectionManager = null;
 let connection = null;
 let connectionQueue = null;
 let documentManager = null;
-let errorHandler = null;
 let goaClient = null;
 let modeController = null;
 let notificationManager = null;

src/trackerController.js
@@ -28,6 +28,7 @@ const Utils = imports.utils;
 
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
+const _ = imports.gettext.gettext;
 
 const QueryType = {
     SELECT: 0,
@@ -154,11 +155,19 @@ const TrackerController = new Lang.Class({
         return this._querying;
     },
 
+    _onQueryError: function(exception) {
+        if (exception.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.CANCELLED))
+            return;
+
+        let message = _("Unable to fetch the list of documents");
+        this.emit('query-error', message, exception);
+    },
+
     _onQueryFinished: function(exception) {
         this._setQueryStatus(false);
 
         if (exception)
-            Global.errorHandler.addQueryError(exception);
+            this._onQueryError(exception);
         else
             Global.offsetController.resetItemCount();
 

commit 76e33267e99905de222f9c6ffe2d07df04340135

embed: Remove unused variable
Fallout from 53440cfe76440a759bd44385d45d07801e9f871b

https://bugzilla.gnome.org/show_bug.cgi?id=744643

src/embed.js
@@ -204,7 +204,6 @@ const Embed = new Lang.Class({
     Name: 'Embed',
 
     _init: function() {
-        this._queryErrorId = 0;
         this._noResultsChangeId = 0;
         this._loadShowId = 0;
 