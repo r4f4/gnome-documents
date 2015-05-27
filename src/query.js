r4f4/gnome-documents

commit 17322bb4b627ebfef0b8c36ba22d390d0910faa6

Add support for eBooks
Add a new application, "Books" (aka gnome-books) to the
gnome-documents git repository. This application shares most
of its code with Documents, and it supports:
- Categorising e-Books vs. Comics
- Searches
- Preview of CBZ/CBR/etc. comic formats (supported through the
  libevince backend) with "fit page" zoom by default

https://bugzilla.gnome.org/show_bug.cgi?id=704316

src/query.js
@@ -44,7 +44,8 @@ const QueryFlags = {
     UNFILTERED: 1 << 0
 };
 
-const LOCAL_COLLECTIONS_IDENTIFIER = 'gd:collection:local:';
+const LOCAL_DOCUMENTS_COLLECTIONS_IDENTIFIER = 'gd:collection:local:';
+const LOCAL_BOOKS_COLLECTIONS_IDENTIFIER = 'gb:collection:local:';
 
 const QueryBuilder = new Lang.Class({
     Name: 'QueryBuilder',
@@ -204,11 +205,17 @@ const QueryBuilder = new Lang.Class({
     },
 
     buildCreateCollectionQuery: function(name) {
+        let application = Gio.Application.get_default();
+        let collectionsIdentifier;
+        if (!application.isBooks)
+            collectionsIdentifier = LOCAL_DOCUMENTS_COLLECTIONS_IDENTIFIER;
+        else
+            collectionsIdentifier = LOCAL_BOOKS_COLLECTIONS_IDENTIFIER;
         let time = GdPrivate.iso8601_from_timestamp(GLib.get_real_time() / GLib.USEC_PER_SEC);
         let sparql = ('INSERT { _:res a nfo:DataContainer ; a nie:DataObject ; ' +
                       'nie:contentLastModified \"' + time + '\" ; ' +
                       'nie:title \"' + name + '\" ; ' +
-                      'nao:identifier \"' + LOCAL_COLLECTIONS_IDENTIFIER + name + '\" }');
+                      'nao:identifier \"' + collectionsIdentifier + name + '\" }');
 
         return this._createQuery(sparql);
     },

data/Makefile.am
@@ -8,21 +8,22 @@ appdata_in_files = org.gnome.Documents.appdata.xml.in
 
 desktopdir = $(datadir)/applications
 desktop_DATA = $(desktop_in_files:.desktop.in=.desktop)
-desktop_in_files = org.gnome.Documents.desktop.in
+desktop_in_files = org.gnome.Documents.desktop.in org.gnome.Books.desktop.in
 
 searchproviderdir = $(datadir)/gnome-shell/search-providers
 searchprovider_DATA = org.gnome.Documents.search-provider.ini
 
 @INTLTOOL_DESKTOP_RULE@
 
-check-local: org.gnome.Documents.desktop
+check-local: org.gnome.Documents.desktop org.gnome.Books.desktop
 	$(DESKTOP_FILE_VALIDATE) org.gnome.Documents.desktop
+	$(DESKTOP_FILE_VALIDATE) org.gnome.Books.desktop
 
 gsettings_ENUM_NAMESPACE = org.gnome.Documents
 gsettings_ENUM_FILES = \
     $(top_srcdir)/libgd/libgd/gd-main-view.h
 
-gsettings_SCHEMAS = org.gnome.documents.gschema.xml
+gsettings_SCHEMAS = org.gnome.documents.gschema.xml org.gnome.books.gschema.xml
 .PRECIOUS: $(gsettings_SCHEMAS)
 
 @GSETTINGS_RULES@

data/org.gnome.Books.desktop.in
@@ -1,0 +1,13 @@
+[Desktop Entry]
+_Name=Books
+_Comment=Access, manage and share books
+Exec=gnome-books
+# FIXME icon
+Icon=gnome-books
+Terminal=false
+Type=Application
+StartupNotify=true
+DBusActivatable=true
+OnlyShowIn=GNOME;
+Categories=GNOME;GTK;Utility;Core;
+_Keywords=Books;Comics;ePub;PDF;

data/org.gnome.books.gschema.xml
@@ -1,0 +1,24 @@
+<schemalist gettext-domain="gnome-documents">
+  <schema id="org.gnome.books" path="/org/gnome/books/">
+    <key name="view-as" enum="org.gnome.Documents.GdMainViewType">
+      <default>'icon'</default>
+      <summary>View as</summary>
+      <description>View as type</description>
+    </key>
+    <key name="window-size" type="ai">
+      <default>[768, 600]</default>
+      <summary>Window size</summary>
+      <description>Window size (width and height).</description>
+    </key>
+    <key name="window-position" type="ai">
+      <default>[]</default>
+      <summary>Window position</summary>
+      <description>Window position (x and y).</description>
+    </key>
+    <key name="window-maximized" type="b">
+      <default>true</default>
+      <summary>Window maximized</summary>
+      <description>Window maximized state</description>
+    </key>
+  </schema>
+</schemalist>

src/Makefile-js.am
@@ -6,6 +6,7 @@ dist_js_DATA = \
     edit.js \
     embed.js \
     main.js \
+    mainBooks.js \
     mainToolbar.js \
     mainWindow.js \
     manager.js \

src/Makefile.am
@@ -56,10 +56,14 @@ typelib_DATA += $(gir_DATA:.gir=.typelib)
 
 CLEANFILES += $(gir_DATA) $(typelib_DATA)
 
-service_in_files = org.gnome.Documents.service.in
+service_in_files = org.gnome.Documents.service.in org.gnome.Books.service.in
 servicedir = $(datadir)/dbus-1/services
-service_DATA = org.gnome.Documents.service
+service_DATA = org.gnome.Documents.service org.gnome.Books.service
+
 org.gnome.Documents.service: org.gnome.Documents.service.in
+	$(AM_V_GEN) $(do_subst) $< > $@
+
+org.gnome.Books.service: org.gnome.Books.service.in
 	$(AM_V_GEN) $(do_subst) $< > $@
 
 CLEANFILES += $(service_DATA)
@@ -72,5 +76,13 @@ gnome-documents: gnome-documents.in
 
 CLEANFILES += gnome-documents
 EXTRA_DIST += gnome-documents.in
+
+bin_SCRIPTS += gnome-books
+gnome-books: gnome-books.in
+	$(AM_V_GEN) $(do_subst) $< > $@
+	chmod +x $@
+
+CLEANFILES += gnome-books
+EXTRA_DIST += gnome-books.in
 
 -include $(top_srcdir)/git.mk

src/application.js
@@ -110,17 +110,27 @@ const Application = new Lang.Class({
     Name: 'Application',
     Extends: Gtk.Application,
 
-    _init: function() {
+    _init: function(isBooks) {
         this.minersRunning = [];
         this._activationTimestamp = Gdk.CURRENT_TIME;
         this._extractPriority = null;
 
+        this.isBooks = isBooks;
+
         Gettext.bindtextdomain('gnome-documents', Path.LOCALE_DIR);
         Gettext.textdomain('gnome-documents');
-        GLib.set_prgname('gnome-documents');
-        GLib.set_application_name(_("Documents"));
+        let appid;
+        if (!this.isBooks) {
+            GLib.set_prgname('gnome-documents');
+            GLib.set_application_name(_("Documents"));
+            appid = 'org.gnome.Documents';
+        } else {
+            GLib.set_prgname('gnome-books');
+            GLib.set_application_name(_("Books"));
+            appid = 'org.gnome.Books';
+        }
 
-        this.parent({ application_id: 'org.gnome.Documents',
+        this.parent({ application_id: appid,
                       inactivity_timeout: 12000 });
 
         this._searchProvider = new ShellSearchProvider.ShellSearchProvider();
@@ -409,7 +419,10 @@ const Application = new Lang.Class({
         resource._register();
 
         application = this;
-        settings = new Gio.Settings({ schema_id: 'org.gnome.documents' });
+        if (!application.isBooks)
+            settings = new Gio.Settings({ schema: 'org.gnome.documents' });
+        else
+            settings = new Gio.Settings({ schema: 'org.gnome.books' });
 
         let gtkSettings = Gtk.Settings.get_default();
         gtkSettings.connect('notify::gtk-theme-name', Lang.bind(this, this._themeChanged));
@@ -542,7 +555,9 @@ const Application = new Lang.Class({
 
         this._initActions();
         this._initAppMenu();
-        this._initGettingStarted();
+
+        if (!this.isBooks)
+            this._initGettingStarted();
     },
 
     vfunc_shutdown: function() {

src/documents.js
@@ -879,6 +879,8 @@ const GoogleDocument = new Lang.Class({
             description = _("Spreadsheet");
         else if (this.rdfType.indexOf('nfo#Presentation') != -1)
             description = _("Presentation");
+        else if (this.rdfType.indexOf('nfo#EBook') != -1)
+            description = _("e-Book");
         else
             description = _("Document");
 
@@ -1049,6 +1051,8 @@ const SkydriveDocument = new Lang.Class({
             description = _("Spreadsheet");
         else if (this.rdfType.indexOf('nfo#Presentation') != -1)
             description = _("Presentation");
+        else if (this.rdfType.indexOf('nfo#EBook') != -1)
+            description = _("e-Book");
         else
             description = _("Document");
 

commit 5e9d776ae276cb660104bb19d8d4743c5f2f6747

query: Display collections in "collection dialog"
Fallout from 17322bb4b627ebfef0b8c36ba22d390d0910faa6

https://bugzilla.gnome.org/show_bug.cgi?id=749900

src/query.js
@@ -48,8 +48,7 @@ const QueryFlags = {
     SEARCH: 1 << 3
 };
 
-const LOCAL_DOCUMENTS_COLLECTIONS_IDENTIFIER = 'gd:collection:local:';
-const LOCAL_BOOKS_COLLECTIONS_IDENTIFIER = 'gb:collection:local:';
+const LOCAL_COLLECTIONS_IDENTIFIER = 'gd:collection:local:';
 
 const QueryBuilder = new Lang.Class({
     Name: 'QueryBuilder',
@@ -220,17 +219,11 @@ const QueryBuilder = new Lang.Class({
     },
 
     buildCreateCollectionQuery: function(name) {
-        let application = Gio.Application.get_default();
-        let collectionsIdentifier;
-        if (!application.isBooks)
-            collectionsIdentifier = LOCAL_DOCUMENTS_COLLECTIONS_IDENTIFIER;
-        else
-            collectionsIdentifier = LOCAL_BOOKS_COLLECTIONS_IDENTIFIER;
         let time = GdPrivate.iso8601_from_timestamp(GLib.get_real_time() / GLib.USEC_PER_SEC);
         let sparql = ('INSERT { _:res a nfo:DataContainer ; a nie:DataObject ; ' +
                       'nie:contentLastModified \"' + time + '\" ; ' +
                       'nie:title \"' + name + '\" ; ' +
-                      'nao:identifier \"' + collectionsIdentifier + name + '\" }');
+                      'nao:identifier \"' + LOCAL_COLLECTIONS_IDENTIFIER + name + '\" }');
 
         return this._createQuery(sparql);
     },