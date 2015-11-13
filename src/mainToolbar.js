r4f4/gnome-documents

commit 3934a00179c95fd4890e5fe2f4a0a902c2848599

mainToolbar: Connect to action-state-changed for consistency

src/mainToolbar.js
@@ -165,7 +165,7 @@ const OverviewToolbar = new Lang.Class({
                                                 action_target: GLib.Variant.new('s', 'icon') });
         viewAsBox.add(this._viewGridButton);
 
-        this._viewSettingsId = Application.settings.connect('changed::view-as',
+        this._viewSettingsId = Application.application.connect('action-state-changed::view-as',
             Lang.bind(this, this._updateViewAsButtons));
         this._updateViewAsButtons();
     },

commit 9f3673ae13d37dec5399618bb85f5675886159f9

mainToolbar: Correct disconnect signal
Fallout from 3934a00179c95fd4890e5fe2f4a0a902c2848599

https://bugzilla.gnome.org/show_bug.cgi?id=757860

src/mainToolbar.js
@@ -308,7 +308,7 @@ const OverviewToolbar = new Lang.Class({
         }
 
         if (this._viewSettingsId != 0) {
-            Application.settings.disconnect(this._viewSettingsId);
+            Application.application.disconnect(this._viewSettingsId);
             this._viewSettingsId = 0;
         }
     },