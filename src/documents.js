6edb4199ccb446f4ba40505e0d25d7481c24220b
Bastien Nocera <hadess@hadess.net>
2015-11-24 15:31:55 +0100

    application, documents, edit: Disable Print menu item when unsupported
    When we finish to load a document, verify whether it can be printed
    before enabling the "Print..." menu item.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=758445

--- src/documents.js
+++ src/documents.js
@@ -369,6 +369,13 @@ const DocCommon = new Lang.Class({
         log('Error: DocCommon implementations must override canTrash');
     },
 
+    canPrint: function(docModel) {
+        if (!docModel)
+            return false;
+
+        return EvView.PrintOperation.exists_for_document(docModel.get_document());
+    },
+
     trash: function() {
         if (!this.canTrash())
             return;
@@ -607,6 +614,9 @@ const DocCommon = new Lang.Class({
                     log('Unable to print document ' + this.uri + ': ' + error);
                     return;
                 }
+
+                if (!this.canPrint(docModel))
+                    return;
 
                 let printOp = EvView.PrintOperation.new(docModel.get_document());
 
