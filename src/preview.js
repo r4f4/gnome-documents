r4f4/gnome-documents

commit cd6d7c76ddf063fe0f285044c06ced0ce45e97d7

preview: Fix possible warnings exiting fullscreen
After opening a document, turning on the fullscreen mode, and starting a
search (type a few letters), you'll get warnings from EvView when "Esc"
is pressed.

This is caused by the search being reset when exiting the preview. As we
don't have a document, or document model anymore, there's no search to
be done.

https://bugzilla.gnome.org/show_bug.cgi?id=759248

src/preview.js
@@ -568,6 +568,10 @@ const PreviewView = new Lang.Class({
             this._updateNightMode();
 
             this.set_visible_child_full('view', Gtk.StackTransitionType.NONE);
+        } else {
+            if (!this._jobFind.is_finished())
+                this._jobFind.cancel();
+            this._jobFind = null;
         }
     },
 

commit f5b2047060d9a071a942125d0f837dbc19310bd6

preview: Check if this._jobFind is null
Fallout from cd6d7c76ddf063fe0f285044c06ced0ce45e97d7

https://bugzilla.gnome.org/show_bug.cgi?id=759248

src/preview.js
@@ -569,9 +569,11 @@ const PreviewView = new Lang.Class({
 
             this.set_visible_child_full('view', Gtk.StackTransitionType.NONE);
         } else {
-            if (!this._jobFind.is_finished())
-                this._jobFind.cancel();
-            this._jobFind = null;
+	    if (this._jobFind) {
+	        if (!this._jobFind.is_finished())
+	            this._jobFind.cancel();
+	        this._jobFind = null;
+	    }
         }
     },
 